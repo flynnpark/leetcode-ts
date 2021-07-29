07f31c7f011bedda7a38f12406a0f267d35ea451
Flynn <wphestiraid@gmail.com>
2021-07-29 09:24:15 +0900

    setting: eslint, commitlint, husky 및 tsconfig 설정

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const foldersUnderSrc = fs
+  .readdirSync('src', { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name);
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'jest', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages. `react` related packages come first.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+          // Absolute imports and Relative imports.
+          [`^(${foldersUnderSrc.join('|')})(/.*|$)`, '^\\.'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+  },
+  env: {
+    'jest/globals': true,
+  },
+};
---
+++ src/solutions/0001.ts
@@ -0,0 +1,4 @@
+function twoSum(nums: number[], target: number): number[] {
+  console.log("twoSum");
+  return [];
+}
