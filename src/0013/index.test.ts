09b557037b37390bc1df9f446e6f2efdf278bb22
Flynn <wphestiraid@gmail.com>
2021-08-25 17:33:53 +0900

    problem: 0013. Roman to Integer

---
+++ src/0013/index.test.ts
@@ -0,0 +1,35 @@
+import { romanToInt } from '0013';
+
+test('0013. Roman to Integer', () => {
+  interface TestCase {
+    input: string;
+    output: number;
+  }
+
+  const testCases: TestCase[] = [
+    {
+      input: 'III',
+      output: 3,
+    },
+    {
+      input: 'IV',
+      output: 4,
+    },
+    {
+      input: 'IX',
+      output: 9,
+    },
+    {
+      input: 'LVIII',
+      output: 58,
+    },
+    {
+      input: 'MCMXCIV',
+      output: 1994,
+    },
+  ];
+
+  testCases.forEach(({ input, output }) => {
+    expect(romanToInt(input)).toBe(output);
+  });
+});
