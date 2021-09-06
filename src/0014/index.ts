618ebf94cf397d4f803e8223117ed0ddc6b56cc9
Flynn <wphestiraid@gmail.com>
2021-09-06 15:04:40 +0900

    solution: 0014. Longest Common Prefix

--- src/0014/index.ts
+++ src/0014/index.ts
@@ -1,3 +1,14 @@
-function longestCommonPrefix(strs: string[]): string {}
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) return '';
+
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substr(0, prefix.length - 1);
+      if (prefix.length === 0) return '';
+    }
+  }
+  return prefix;
+}
 
 export { longestCommonPrefix };
