50cf6ad7d04c52c5f4cbcd1112acb397695d88d7
Flynn <wphestiraid@gmail.com>
2021-09-09 19:47:15 +0900

    solution: 0020: Valid Parentheses

--- src/0020/index.ts
+++ src/0020/index.ts
@@ -1,3 +1,24 @@
-function isValid(s: string): boolean {}
+const pair: { [key: string]: string } = {
+  ')': '(',
+  ']': '[',
+  '}': '{',
+};
+
+function isValid(s: string): boolean {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (pair[char]) {
+      if (stack.length === 0 || stack[stack.length - 1] !== pair[char]) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
+}
 
 export { isValid };
