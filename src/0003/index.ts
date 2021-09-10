c1031362def0228d5070e41b3ed755dfd68efb06
Flynn <wphestiraid@gmail.com>
2021-09-10 16:49:15 +0900

    solution: 0003. Longest Substring Without Repeating Characters

--- src/0003/index.ts
+++ src/0003/index.ts
@@ -1,3 +1,16 @@
-function lengthOfLongestSubstring(s: string): number {}
+function lengthOfLongestSubstring(s: string): number {
+  const map = new Map<string, number>();
+  let max = 0;
+  let start = 0;
+  for (let i = 0; i < s.length; i++) {
+    const charIndex = map.get(s[i]);
+    if (charIndex !== undefined) {
+      start = Math.max(start, charIndex + 1);
+    }
+    map.set(s[i], i);
+    max = Math.max(max, i - start + 1);
+  }
+  return max;
+}
 
 export { lengthOfLongestSubstring };
