9dce7afc94ef5040bf3ce2897432d607c1e73ebb
Flynn <wphestiraid@gmail.com>
2023-02-20 09:10:59 +0900

    solution: 0661. Image Smoother

--- src/0661/index.ts
+++ src/0661/index.ts
@@ -1,3 +1,28 @@
-function imageSmoother(img: number[][]): number[][] {}
+function getAverage(matrix: number[][], idx1: number, idx2: number) {
+  const candidates = [-1, 0, 1];
+  let total = 0;
+  let count = 0;
+
+  for (const i of candidates) {
+    for (const j of candidates) {
+      if (matrix[idx1 + i]?.[idx2 + j] !== undefined) {
+        total += matrix[idx1 + i][idx2 + j];
+        count++;
+      }
+    }
+  }
+
+  return Math.floor(total / count);
+}
+
+function imageSmoother(img: number[][]): number[][] {
+  return new Array(img.length)
+    .fill(0)
+    .map((_, idx1) =>
+      new Array(img[idx1].length)
+        .fill(0)
+        .map((_, idx2) => getAverage(img, idx1, idx2)),
+    );
+}
 
 export { imageSmoother };
