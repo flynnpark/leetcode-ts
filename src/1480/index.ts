078f6d47ff3566d8281a079f82eb31f05643d765
Flynn <wphestiraid@gmail.com>
2023-02-21 09:19:58 +0900

    solution: 1480. Running Sum of 1d Array

--- src/1480/index.ts
+++ src/1480/index.ts
@@ -1,3 +1,7 @@
-function runningSum(nums: number[]): number[] {}
+function runningSum(nums: number[]): number[] {
+  return nums.map((_, index) =>
+    nums.slice(0, index + 1).reduce((a, b) => a + b, 0),
+  );
+}
 
 export { runningSum };
