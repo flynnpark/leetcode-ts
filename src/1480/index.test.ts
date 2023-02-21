4de2d8e5cec1374e3cef9dc9b559480de68ed756
Flynn <wphestiraid@gmail.com>
2023-02-21 09:17:22 +0900

    problem: 1480. Running Sum of 1d Array

---
+++ src/1480/index.test.ts
@@ -0,0 +1,35 @@
+import { runningSum } from '.';
+
+describe('0027. Remove Element', () => {
+  interface TestCase {
+    input: {
+      nums: number[];
+    };
+    output: number[];
+  }
+
+  const testCases: TestCase[] = [
+    {
+      input: {
+        nums: [1, 2, 3, 4],
+      },
+      output: [1, 3, 6, 10],
+    },
+    {
+      input: {
+        nums: [1, 1, 1, 1, 1],
+      },
+      output: [1, 2, 3, 4, 5],
+    },
+    {
+      input: {
+        nums: [3, 1, 2, 10, 1],
+      },
+      output: [3, 4, 6, 16, 17],
+    },
+  ];
+
+  test.each(testCases)('%o', ({ input: { nums }, output }) => {
+    expect(runningSum(nums)).toEqual(output);
+  });
+});
