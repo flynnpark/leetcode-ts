3ee052ee973eed1a2288b09f5aed599de9d10b7c
Flynn <wphestiraid@gmail.com>
2023-02-27 13:54:13 +0900

    solution: 0724. Find Pivot Index

--- src/0724/index.ts
+++ src/0724/index.ts
@@ -1,3 +1,16 @@
-function pivotIndex(nums: number[]): number {}
+function pivotIndex(nums: number[]): number {
+  let left = 0;
+  let right = nums.reduce((a, b) => a + b, 0);
+
+  for (let index = 0; index < nums.length; index++) {
+    right -= nums[index];
+    if (left === right) {
+      return index;
+    }
+    left += nums[index];
+  }
+
+  return -1;
+}
 
 export { pivotIndex };
