3c2b7eafdc1f3c4e38b499f1f4c89455f5286889
Flynn <wphestiraid@gmail.com>
2023-03-13 09:20:26 +0900

    solution: 0876. Middle of the Linked List

--- src/0876/index.ts
+++ src/0876/index.ts
@@ -1,5 +1,15 @@
 import { ListNode } from '../utils';
 
-function middleNode(head: ListNode | null): ListNode | null {}
+function middleNode(head: ListNode | null): ListNode | null {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast !== null && fast.next !== null && slow !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
 
 export { middleNode };
