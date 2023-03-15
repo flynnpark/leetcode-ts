21a1a0bdbc3fe023dbbcbb830a05c23c9e86d328
Flynn <wphestiraid@gmail.com>
2023-03-15 21:29:23 +0900

    solution: 0409. Longest Palindrome

--- src/0409/index.ts
+++ src/0409/index.ts
@@ -1,3 +1,25 @@
-function longestPalindrome(s: string): number {}
+function longestPalindrome(s: string): number {
+  const freq = new Map<string, number>();
+  for (const c of s) {
+    freq.set(c, (freq.get(c) || 0) + 1);
+  }
+
+  let begin = '';
+  let end = '';
+  let mid = '';
+
+  for (const [key, value] of freq) {
+    if (value % 2 === 0) {
+      begin += key.repeat(value / 2);
+      end = key.repeat(value / 2) + end;
+    } else {
+      begin += key.repeat(Math.floor(value / 2));
+      end = key.repeat(Math.floor(value / 2)) + end;
+      mid = key;
+    }
+  }
+
+  return (begin + mid + end).length;
+}
 
 export { longestPalindrome };
