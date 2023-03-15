09d48b4b46061a2d81b825ef30921ca73c9f5ea7
Flynn <wphestiraid@gmail.com>
2023-03-15 21:11:53 +0900

    solution: 0121. Best Time to Buy and Sell Stock

--- src/0121/index.ts
+++ src/0121/index.ts
@@ -1,3 +1,19 @@
-function maxProfit(prices: number[]): number {}
+function maxProfit(prices: number[]): number {
+  let minIndex = 0;
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < prices[minIndex]) {
+      minIndex = i;
+    }
+
+    const profit = prices[i] - prices[minIndex];
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+  }
+
+  return maxProfit;
+}
 
 export { maxProfit };
