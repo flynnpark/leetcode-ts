4a6ca08823810d44f4e4980db4a1c8a6c86a2b8d
Flynn <wphestiraid@gmail.com>
2023-03-21 08:38:02 +0900

    solution: 0102. Binary Tree Level Order Traversal

--- src/0102/index.ts
+++ src/0102/index.ts
@@ -1,8 +1,24 @@
 import { TreeNode } from '../utils';
 
 function levelOrder(root: TreeNode | null): number[][] {
-  console.log(root);
-  return [];
+  const dict: { [key: number]: number[] } = {};
+
+  function bfs(tree: TreeNode | null, level: number) {
+    if (!tree) return [];
+
+    if (!dict[level]) {
+      dict[level] = [tree.val];
+    } else {
+      dict[level].push(tree.val);
+    }
+
+    if (tree.left) bfs(tree.left, level + 1);
+    if (tree.right) bfs(tree.right, level + 1);
+  }
+
+  bfs(root, 0);
+
+  return Object.values(dict);
 }
 
 export { levelOrder };
