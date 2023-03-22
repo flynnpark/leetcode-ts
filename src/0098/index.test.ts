0648e58bfd6c53a3003aa90918a067cc9f152c3c
Flynn <wphestiraid@gmail.com>
2023-03-23 08:39:49 +0900

    problem: 0098. Validate Binary Search Tree

---
+++ src/0098/index.test.ts
@@ -0,0 +1,29 @@
+import { transformListToTreeNode } from '../utils';
+import { isValidBST } from '.';
+
+test('0098. Validate Binary Search Tree', () => {
+  interface TestCase {
+    input: { root: (number | null)[] };
+    output: boolean;
+  }
+
+  const testCases: TestCase[] = [
+    {
+      input: {
+        root: [2, 1, 3],
+      },
+      output: true,
+    },
+    {
+      input: {
+        root: [5, 1, 4, null, null, 3, 6],
+      },
+      output: false,
+    },
+  ];
+
+  testCases.forEach(({ input: { root }, output }) => {
+    const transformedRoot = transformListToTreeNode(root);
+    expect(isValidBST(transformedRoot)).toEqual(output);
+  });
+});
---
+++ src/0098/index.ts
@@ -0,0 +1,5 @@
+import { TreeNode } from '../utils';
+
+function isValidBST(root: TreeNode | null): boolean {}
+
+export { isValidBST };
