c9e9eb4c4437af7d78dc8148aea83e3b35b4a300
Flynn <wphestiraid@gmail.com>
2023-03-27 10:15:48 +0900

    solution: 1615. Maximal Network Rank

--- src/1615/index.ts
+++ src/1615/index.ts
@@ -1,3 +1,25 @@
-function maximalNetworkRank(n: number, roads: number[][]): number {}
+function maximalNetworkRank(n: number, roads: number[][]): number {
+  const network: { [key: string]: number[] } = {};
+  for (let i = 0; i < n; i++) {
+    network[i] = [];
+  }
+  for (const [a, b] of roads) {
+    network[a].push(b);
+    network[b].push(a);
+  }
+
+  let maxNetwork = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      let currentNetwork = network[i].length + network[j].length;
+      if (network[i].includes(j) || network[j].includes(i)) {
+        currentNetwork--;
+      }
+      maxNetwork = Math.max(maxNetwork, currentNetwork);
+    }
+  }
+
+  return maxNetwork;
+}
 
 export { maximalNetworkRank };
