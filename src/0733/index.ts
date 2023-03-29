183ce09e78bfaa7cb2d4b33f0df3e7e6c18cb73b
Flynn <wphestiraid@gmail.com>
2023-03-30 08:23:57 +0900

    solution: 0733. Flood Fill

--- src/0733/index.ts
+++ src/0733/index.ts
@@ -3,6 +3,42 @@ function floodFill(
   sr: number,
   sc: number,
   color: number,
-): number[][] {}
+): number[][] {
+  const targetColor = image[sr][sc];
+
+  function dfs(i: number, j: number) {
+    if (
+      i < 0 ||
+      i >= image.length ||
+      j < 0 ||
+      j >= image[i].length ||
+      image[i][j] === color ||
+      image[i][j] !== targetColor
+    ) {
+      return;
+    }
+
+    if (
+      ([
+        image[i - 1]?.[j],
+        image[i + 1]?.[j],
+        image[i][j - 1],
+        image[i][j + 1],
+      ].includes(color) ||
+        (i === sr && j === sc)) &&
+      image[i][j] === targetColor
+    ) {
+      image[i][j] = color;
+    }
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  }
+
+  dfs(sr, sc);
+
+  return image;
+}
 
 export { floodFill };
