97481c6887009fe59e7b5fec241ad419f695c871
Flynn <wphestiraid@gmail.com>
2023-03-30 08:26:32 +0900

    solution: 0733. Flood Fill
    - 간결하게 수정

--- src/0733/index.ts
+++ src/0733/index.ts
@@ -12,29 +12,20 @@ function floodFill(
       i >= image.length ||
       j < 0 ||
       j >= image[i].length ||
-      image[i][j] === color ||
       image[i][j] !== targetColor
     ) {
       return;
     }
 
-    if (
-      ([
-        image[i - 1]?.[j],
-        image[i + 1]?.[j],
-        image[i][j - 1],
-        image[i][j + 1],
-      ].includes(color) ||
-        (i === sr && j === sc)) &&
-      image[i][j] === targetColor
-    ) {
-      image[i][j] = color;
-    }
+    image[i][j] = color;
+
     dfs(i + 1, j);
     dfs(i - 1, j);
     dfs(i, j + 1);
     dfs(i, j - 1);
   }
+
+  if (image[sr][sc] === color) return image;
 
   dfs(sr, sc);
 
