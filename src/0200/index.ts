f23ebeb64cd2fdebbc30279d8f8d4e4e01f28d07
Flynn <wphestiraid@gmail.com>
2023-03-31 08:54:01 +0900

    solution: 0200. Number of Islands

--- src/0200/index.ts
+++ src/0200/index.ts
@@ -1,3 +1,32 @@
-function numIslands(grid: string[][]): number {}
+function numIslands(grid: string[][]): number {
+  function dfs(i: number, j: number) {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[i].length ||
+      grid[i][j] === '0'
+    )
+      return;
+    grid[i][j] = '0';
+
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j - 1);
+    dfs(i, j + 1);
+  }
+
+  let result = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        result++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return result;
+}
 
 export { numIslands };
