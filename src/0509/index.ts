e8cd33354dfb3c73b8dcfced2ad58dbd1a57d18e
Flynn <wphestiraid@gmail.com>
2023-04-06 13:51:33 +0900

    solution: 0509. Fibonacci Number

--- src/0509/index.ts
+++ src/0509/index.ts
@@ -1,3 +1,11 @@
-function fib(n: number): number {}
+function fib(n: number): number {
+  function dp(n: number, temp: number[]): number {
+    console.log(n, temp);
+    if (n <= 1) return n;
+    if (temp[n] != null) return temp[n];
+    return (temp[n] = dp(n - 1, temp) + dp(n - 2, temp));
+  }
+  return dp(n, [n + 1]);
+}
 
 export { fib };
