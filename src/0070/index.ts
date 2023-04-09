321c9008ad26f2acc8bce5cd81440a0b7bfe2441
Flynn <wphestiraid@gmail.com>
2023-04-09 11:33:58 +0900

    solution: 0070. Climbing Stairs

--- src/0070/index.ts
+++ src/0070/index.ts
@@ -1,3 +1,15 @@
-function climbStairs(n: number): number {}
+function climbStairs(n: number): number {
+  if (n < 3) {
+    return n;
+  }
+
+  const memory = [1, 2];
+
+  for (let i = 2; i < n; i++) {
+    memory[i] = memory[i - 1] + memory[i - 2];
+  }
+
+  return memory[n - 1];
+}
 
 export { climbStairs };
--- src/0509/index.ts
+++ src/0509/index.ts
@@ -1,6 +1,5 @@
 function fib(n: number): number {
   function dp(n: number, temp: number[]): number {
-    console.log(n, temp);
     if (n <= 1) return n;
     if (temp[n] != null) return temp[n];
     return (temp[n] = dp(n - 1, temp) + dp(n - 2, temp));
