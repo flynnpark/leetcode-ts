9becfcc45b8e3df9d07a09d7a77011bc7c0124be
Flynn <wphestiraid@gmail.com>
2023-04-17 08:40:06 +0900

    problem: 0019. Remove Nth Node From End of List

---
+++ src/0019/index.test.ts
@@ -0,0 +1,44 @@
+import { transformListNodeToList, transformListToListNode } from '../utils';
+import { removeNthFromEnd } from '.';
+
+describe('0019. Remove Nth Node From End of List', () => {
+  interface TestCase {
+    input: {
+      head: number[];
+      n: number;
+    };
+    output: number[];
+  }
+
+  const testCases: TestCase[] = [
+    {
+      input: {
+        head: [1, 2, 3, 4, 5],
+        n: 2,
+      },
+      output: [1, 2, 3, 5],
+    },
+    {
+      input: {
+        head: [1],
+        n: 1,
+      },
+      output: [],
+    },
+    {
+      input: {
+        head: [1, 2],
+        n: 1,
+      },
+      output: [1],
+    },
+  ];
+
+  test.each(testCases)('%o', ({ input, output }) => {
+    const transformedHead = transformListToListNode(input.head);
+    const transformedResult = transformListNodeToList(
+      removeNthFromEnd(transformedHead, input.n),
+    );
+    expect(transformedResult).toBe(output);
+  });
+});
---
+++ src/0019/index.ts
@@ -0,0 +1,5 @@
+import { ListNode } from '../utils';
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {}
+
+export { removeNthFromEnd };
