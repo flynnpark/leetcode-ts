288de1bf7fd5ecbff60e425f4177a3b05c6f7c2f
Flynn <wphestiraid@gmail.com>
2023-04-21 08:58:46 +0900

    solution: 0344. Reverse String

--- src/0344/index.ts
+++ src/0344/index.ts
@@ -1,6 +1,17 @@
 /**
  Do not return anything, modify s in-place instead.
  */
-function reverseString(s: string[]): void {}
+function reverseString(s: string[]): void {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+}
 
 export { reverseString };
