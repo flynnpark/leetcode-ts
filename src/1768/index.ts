c602810602848c544025cdfbd74e58b4bf5e5c91
Flynn <wphestiraid@gmail.com>
2023-04-24 08:55:11 +0900

    solution: 1768. Merge Strings Alternately

--- src/1768/index.ts
+++ src/1768/index.ts
@@ -1,3 +1,13 @@
-function mergeAlternately(word1: string, word2: string): string {}
+function mergeAlternately(word1: string, word2: string): string {
+  const maxLength = word1.length > word2.length ? word1.length : word2.length;
+  const result: string[] = [];
+
+  for (let i = 0; i < maxLength; i++) {
+    if (word1[i]) result.push(word1[i]);
+    if (word2[i]) result.push(word2[i]);
+  }
+
+  return result.join('');
+}
 
 export { mergeAlternately };
