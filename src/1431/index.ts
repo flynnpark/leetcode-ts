bc7d92cf903411c8f2f7f798a02a2cf501735727
Flynn <wphestiraid@gmail.com>
2023-05-02 08:27:15 +0900

    solution: 1431. Kids With the Greatest Number of Candies

--- src/1431/index.ts
+++ src/1431/index.ts
@@ -1,3 +1,16 @@
-function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {}
+function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+  const max = Math.max(...candies);
+  const result: boolean[] = [];
+
+  for (const candy of candies) {
+    if (candy + extraCandies >= max) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+
+  return result;
+}
 
 export { kidsWithCandies };
