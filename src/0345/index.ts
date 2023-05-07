30b6ce034e2692ab9641503c00bcc5a472392d11
Flynn <wphestiraid@gmail.com>
2023-05-07 20:02:23 +0900

    solution: 0345. Reverse Vowels of a String

--- src/0345/index.ts
+++ src/0345/index.ts
@@ -1,3 +1,28 @@
-function reverseVowels(s: string): string {}
+function reverseVowels(s: string): string {
+  const vowels = 'aeiouAEIOU';
+  const result = s.split('');
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (
+      vowels.indexOf(result[left]) >= 0 &&
+      vowels.indexOf(result[right]) >= 0
+    ) {
+      const temp = result[left];
+      result[left] = result[right];
+      result[right] = temp;
+      left++;
+      right--;
+    } else if (vowels.indexOf(result[left]) < 0) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return result.join('');
+}
 
 export { reverseVowels };
