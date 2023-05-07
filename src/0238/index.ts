cb1071da961dc13d6a948226477be6d343a20042
Flynn <wphestiraid@gmail.com>
2023-05-08 08:53:31 +0900

    solution: 0238. Product of Array Except Self

--- src/0238/index.ts
+++ src/0238/index.ts
@@ -1,3 +1,29 @@
-function productExceptSelf(nums: number[]): number[] {}
+function productExceptSelf(nums: number[]): number[] {
+  let zeroCount = 0;
+  let maxProduct = 1;
+  const result = [];
+
+  for (const num of nums) {
+    if (num === 0) {
+      zeroCount++;
+    } else {
+      maxProduct *= num;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      if (zeroCount === 1) {
+        result.push(maxProduct);
+      } else {
+        result.push(0);
+      }
+    } else {
+      result.push(zeroCount === 0 ? maxProduct / nums[i] : 0);
+    }
+  }
+
+  return result;
+}
 
 export { productExceptSelf };
