0b17dd84a4972c92bc5a182eb21a1d244b6f231b
Flynn <wphestiraid@gmail.com>
2023-05-07 19:38:33 +0900

    solution: 0605. Can Place Flowers

--- src/0605/index.ts
+++ src/0605/index.ts
@@ -1,3 +1,20 @@
-function canPlaceFlowers(flowerbed: number[], n: number): boolean {}
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  let blankCount = 1;
+  let availableBeds = 0;
+
+  for (const bed of flowerbed) {
+    if (bed) {
+      // bed가 1인 경우
+      availableBeds += Math.floor((blankCount - 1) / 2);
+      blankCount = 0;
+    } else {
+      blankCount += 1;
+    }
+  }
+
+  availableBeds += Math.floor(blankCount / 2);
+
+  return availableBeds >= n;
+}
 
 export { canPlaceFlowers };
