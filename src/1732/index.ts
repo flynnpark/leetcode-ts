8dd58228318e32ba63ce69248a7ea0657adb48de
Flynn <wphestiraid@gmail.com>
2023-05-26 08:44:43 +0900

    solution: 1732. Find the Highest Altitude

--- src/1732/index.ts
+++ src/1732/index.ts
@@ -1,3 +1,13 @@
-function largestAltitude(gain: number[]): number {}
+function largestAltitude(gain: number[]): number {
+  let hightAltitude = 0;
+  let currentAltitude = 0;
+
+  for (let i = 0; i < gain.length; i++) {
+    currentAltitude += gain[i];
+    hightAltitude = Math.max(currentAltitude, hightAltitude);
+  }
+
+  return hightAltitude;
+}
 
 export { largestAltitude };
