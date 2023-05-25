2323aeae957d5a29c63ad6f0c34392811a968ba4
Flynn <wphestiraid@gmail.com>
2023-05-25 09:23:58 +0900

    solution: 1004. Max Consecutive Ones III

--- src/1004/index.ts
+++ src/1004/index.ts
@@ -1,3 +1,27 @@
-function longestOnes(nums: number[], k: number): number {}
+function longestOnes(nums: number[], k: number): number {
+  let maxLength = 0;
+  let zeroCount = 0;
+  let left = 0;
+  let right = 0;
+
+  while (right < nums.length) {
+    if (nums[right] === 0) {
+      zeroCount++;
+    }
+    if (zeroCount <= k) {
+      const currentLength = right - left + 1;
+      if (currentLength > maxLength) {
+        maxLength = currentLength;
+      }
+    } else if (zeroCount > k) {
+      if (nums[left] === 0) {
+        zeroCount--;
+      }
+      left++;
+    }
+    right++;
+  }
+  return maxLength;
+}
 
 export { longestOnes };
