62b26851dde92deb6d597ec004347ccef4f9507f
Flynn <wphestiraid@gmail.com>
2023-05-29 11:26:59 +0900

    solution: 1207. Unique Number of Occurrences

--- src/1207/index.ts
+++ src/1207/index.ts
@@ -1,3 +1,13 @@
-function uniqueOccurrences(arr: number[]): boolean {}
+function uniqueOccurrences(arr: number[]): boolean {
+  const arrMap: { [key: number]: number } = {};
+  for (const num of arr) {
+    arrMap[num] = arrMap[num] === undefined ? 1 : arrMap[num] + 1;
+  }
+
+  return (
+    Object.values(arrMap).length ===
+    Array.from(new Set(Object.values(arrMap))).length
+  );
+}
 
 export { uniqueOccurrences };
