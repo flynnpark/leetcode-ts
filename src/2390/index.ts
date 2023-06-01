e191bc6724a369768d21b6f8eb7996a30c1f239e
Flynn <wphestiraid@gmail.com>
2023-06-02 08:25:13 +0900

    solution: 2390. Removing Stars From a String

--- src/2390/index.ts
+++ src/2390/index.ts
@@ -1,3 +1,14 @@
-function removeStars(s: string): string {}
+function removeStars(s: string): string {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (char !== '*') {
+      stack.push(char);
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.join('');
+}
 
 export { removeStars };
