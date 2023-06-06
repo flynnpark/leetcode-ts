cf8061e23bd2e03d0d81b9613a544586f348a5a4
Flynn <wphestiraid@gmail.com>
2023-06-06 09:31:13 +0900

    solution: 0394. Decode String

--- src/0394/index.ts
+++ src/0394/index.ts
@@ -1,3 +1,24 @@
-function decodeString(s: string): string {}
+function decodeString(s: string): string {
+  const stack: [number, string][] = [];
+  let result = '';
+  let repeatCount = 0;
+
+  for (const char of s) {
+    if (char === '[') {
+      stack.push([repeatCount, result]);
+      repeatCount = 0;
+      result = '';
+    } else if (char === ']' && stack.length) {
+      const [prevRepeatCount, prevResult] = stack.pop() as [number, string];
+      result = prevResult + result.repeat(prevRepeatCount);
+    } else if (!isNaN(Number(char))) {
+      repeatCount = repeatCount * 10 + parseInt(char, 10);
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
 
 export { decodeString };
