4d45b73dc57dde9c49b7c593c96648b9b2c3a7e2
Flynn <wphestiraid@gmail.com>
2023-06-08 08:37:16 +0900

    solution: 0933. Number of Recent Calls

--- src/0933/index.ts
+++ src/0933/index.ts
@@ -1,7 +1,13 @@
 class RecentCounter {
-  constructor() {}
+  queue: number[] = [];
 
-  ping(t: number): number {}
+  ping(t: number): number {
+    while (this.queue.length && this.queue[0] < t - 3000) {
+      this.queue.shift();
+    }
+    this.queue.push(t);
+    return this.queue.length;
+  }
 }
 
 /**
@@ -9,4 +15,5 @@ class RecentCounter {
  * var obj = new RecentCounter()
  * var param_1 = obj.ping(t)
  */
+
 export { RecentCounter };
