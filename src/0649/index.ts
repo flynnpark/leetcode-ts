088908037a47627fcc4457551c4947663a2645f7
Flynn <wphestiraid@gmail.com>
2023-06-13 08:57:50 +0900

    solution: 0649. Dota2 Senate

--- src/0649/index.ts
+++ src/0649/index.ts
@@ -1,3 +1,27 @@
-function predictPartyVictory(senate: string): string {}
+function predictPartyVictory(senate: string): string {
+  const length = senate.length;
+  const rQueue = [];
+  const dQueue = [];
+
+  for (let i = 0; i < length; i++) {
+    if (senate[i] === 'R') {
+      rQueue.push(i);
+    } else {
+      dQueue.push(i);
+    }
+  }
+
+  while (rQueue.length > 0 && dQueue.length > 0) {
+    const r = rQueue.shift() as number;
+    const d = dQueue.shift() as number;
+    if (r < d) {
+      rQueue.push(r + length);
+    } else {
+      dQueue.push(d + length);
+    }
+  }
+  console.log(rQueue, dQueue);
+  return rQueue.length ? 'Radiant' : 'Dire';
+}
 
 export { predictPartyVictory };
