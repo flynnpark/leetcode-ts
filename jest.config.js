52c6ebcc0c8f9cc1f1ec218a9d0356f0008ad7a3
Flynn <wphestiraid@gmail.com>
2024-01-02 09:03:24 +0900

    setting: 패키지 버전 업데이트 및 노드 버전 설정, jest 설정 추가

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testEnvironment: 'jest-environment-node',
+  testMatch: [
+    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
