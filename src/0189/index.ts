2ac05071c2ebb7454440b8de33ae7d4b852c6774
Flynn <wphestiraid@gmail.com>
2024-01-05 09:47:20 +0900

    solution: 0189. Rotate Array

--- src/0189/index.ts
+++ src/0189/index.ts
@@ -1,6 +1,30 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
-function rotate(nums: number[], k: number): void {}
+function rotate(nums: number[], k: number): void {
+  k = k % nums.length;
+  let left = 0;
+  let right = nums.length - 1;
+  nums = reverseArray(nums, left, right);
+
+  left = 0;
+  right = k - 1;
+  nums = reverseArray(nums, left, right);
+
+  left = k;
+  right = nums.length - 1;
+  nums = reverseArray(nums, left, right);
+}
+
+function reverseArray(nums: number[], left: number, right: number): number[] {
+  while (left < right) {
+    const temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
+    left++;
+    right--;
+  }
+  return nums;
+}
 
 export { rotate };
