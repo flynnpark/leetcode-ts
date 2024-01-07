23641b72d0050a2edffef588b0f968d4db48c4eb
Flynn <wphestiraid@gmail.com>
2024-01-08 08:31:56 +0900

    solution: 0122. Best Time to Buy and Sell Stock II

--- src/0122/index.ts
+++ src/0122/index.ts
@@ -1,3 +1,17 @@
-function maxProfit(prices: number[]): number {}
+function maxProfit(prices: number[]): number {
+  let profit: number = 0;
+  let left = 0;
+  let right = 1;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      profit += prices[right] - prices[left];
+      left++;
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return profit;
+}
 
 export { maxProfit };
