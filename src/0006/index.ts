3529f5eb22aaa5eeffac5dc46773f6e55ad3b331
Flynn <wphestiraid@gmail.com>
2024-02-01 09:26:51 +0900

    solution: 0006. Zigzag Conversion

--- src/0006/index.ts
+++ src/0006/index.ts
@@ -1,3 +1,26 @@
-function convert(s: string, numRows: number): string {}
+function convert(s: string, numRows: number): string {
+  if (numRows === 1) {
+    return s;
+  }
+
+  const rows = new Array(numRows).fill('');
+
+  let currentRow = 0;
+  let goingDown = true;
+
+  for (const char of s) {
+    rows[currentRow] += char;
+
+    if (currentRow === 0) {
+      goingDown = true;
+    } else if (currentRow === numRows - 1) {
+      goingDown = false;
+    }
+
+    currentRow += goingDown ? 1 : -1;
+  }
+
+  return rows.join('');
+}
 
 export { convert };
