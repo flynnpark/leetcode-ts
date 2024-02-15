dd84ba2dfe22938254a3349fcd2eae57f2dcb940
Flynn <wphestiraid@gmail.com>
2024-02-16 08:51:25 +0900

    solution: 0383. Ransom Note

--- src/0383/index.ts
+++ src/0383/index.ts
@@ -1,3 +1,19 @@
-function canConstruct(ransomNote: string, magazine: string): boolean {}
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  const map = new Map<string, number>();
+
+  for (const char of magazine) {
+    map.set(char, (map.get(char) ?? 0) + 1);
+  }
+
+  for (const char of ransomNote) {
+    if (map.get(char)) {
+      map.set(char, map.get(char)! - 1);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 export { canConstruct };
